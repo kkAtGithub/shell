471c5cd5d87c0a1909c268cbb4ba0a5cfe80cd06
kk <44739720+kkAtGithub@users.noreply.github.com>
2021-04-13 13:24:32 +0800

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-if [ $# -eq 3 ] || [ $# -eq 4 ]; then
+if [ $# -eq 3 ]; then
     DRIVE_LIST=$(rclone config show | grep "\[.*\]" | sed 's/^.//' | sed 's/.$//' | xargs)
     SRC_PATH=$(echo "$2" | sed 's/ /\\ /g')
     DES_PATH=$(echo "$3" | sed 's/ /\\ /g')
-    if [ $# -eq 4 ]; then
-        DRIVE_LIST=$(cat $4 | grep "\[.*\]" | sed 's/^.//' | sed 's/.$//' | xargs)
-        CONFIG_FILE="--config $4"
-    fi
     for drive in $DRIVE_LIST
     do
-        rclone -P $1 "$SRC_PATH" "$drive":"$DES_PATH" $CONFIG_FILE
+        rclone -P $1 "$SRC_PATH" "$drive":"$DES_PATH"
     done
 fi
