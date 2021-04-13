603c0399ea219118a1c8e356a0ebcc2d21d47cd2
kk <44739720+kkAtGithub@users.noreply.github.com>
2021-04-13 12:52:11 +0800

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ $# -eq 3] || [ $# -eq 4 ]; then
-    apt-get update && \
-    apt-get install rclone -y
-    DRIVE_LIST = $(rclone config show | grep "\[.*\]" | sed 's/^.//' | sed 's/.$//' | xargs)
+if [ $# -eq 3 ] || [ $# -eq 4 ]; then
+    DRIVE_LIST=$(rclone config show | grep "\[.*\]" | sed 's/^.//' | sed 's/.$//' | xargs)
+    SRC_PATH=$(echo "$2" | sed 's/ /\\ /g')
+    DES_PATH=$(echo "$3" | sed 's/ /\\ /g')
     if [ $# -eq 4 ]; then
-        DRIVE_LIST = $(cat $4 | grep "\[.*\]" | sed 's/^.//' | sed 's/.$//' | xargs)
-        CONFIG_FILE = "--config $4"
+        DRIVE_LIST=$(cat $4 | grep "\[.*\]" | sed 's/^.//' | sed 's/.$//' | xargs)
+        CONFIG_FILE="--config $4"
     fi
-    for drive in DRIVE_LIST
-    do 
-        rclone -P $1 "$2" "$drive":"$3" $CONFIG_FILE
+    for drive in $DRIVE_LIST
+    do
+        rclone -P $1 "$SRC_PATH" "$drive":"$DES_PATH" $CONFIG_FILE
     done
 fi
