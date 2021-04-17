8d10348298c191379f75708b996bd5d52d7ce173
kk <44739720+kkAtGithub@users.noreply.github.com>
2021-04-18 02:50:51 +0800

    Update clone_repo.sh

--- clone_repo.sh
+++ clone_repo.sh
@@ -2,9 +2,6 @@
 
 cd /root/shell
 
-export http_proxy=http://127.0.0.1:7890
-export https_proxy=http://127.0.0.1:7890
-
 wget -O docker_setup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/docker_setup.sh && \
 chmod 700 docker_setup.sh
 wget -O fail2ban_setup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/fail2ban_setup.sh && \
