fa4ef9843a3f7adfb2ce278bfe8080e7361b0df1
kk <44739720+kkAtGithub@users.noreply.github.com>
2021-04-18 16:10:59 +0800

    Update fail2ban_setup.sh

--- fail2ban_setup.sh
+++ fail2ban_setup.sh
@@ -2,7 +2,7 @@
 
 apt install fail2ban -y
 
-wget -O /etc/fail2ban/jail.local https://raw.githubusercontent.com/kkAtGithub/shell/main/jail.local
+curl -o /etc/fail2ban/jail.local https://raw.githubusercontent.com/kkAtGithub/shell/main/jail.local
 
 systemctl enable fail2ban && \
 systemctl restart fail2ban
