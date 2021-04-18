58fdcd6e9d38aaa6895ad834fbb6c14ba6624dd8
kk <44739720+kkAtGithub@users.noreply.github.com>
2021-04-18 16:10:32 +0800

    Update clone_repo.sh

--- clone_repo.sh
+++ clone_repo.sh
@@ -2,15 +2,15 @@
 
 cd /root/shell
 
-wget -O docker_setup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/docker_setup.sh && \
+curl -o docker_setup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/docker_setup.sh && \
 chmod 700 docker_setup.sh
-wget -O fail2ban_setup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/fail2ban_setup.sh && \
+curl -o fail2ban_setup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/fail2ban_setup.sh && \
 chmod 700 fail2ban_setup.sh
-wget -O wireguard_config.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/wireguard_config.sh && \
+curl -o wireguard_config.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/wireguard_config.sh && \
 chmod 700 wireguard_config.sh
-wget -O iptables_config.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/iptables_config.sh && \
+curl -o iptables_config.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/iptables_config.sh && \
 chmod 700 iptables_config.sh
-wget -O clone_repo.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/clone_repo.sh && \
+curl -o clone_repo.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/clone_repo.sh && \
 chmod 700 clone_repo.sh
 
 exit 0
