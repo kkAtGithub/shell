2033b27a30d654220fb4be2d5fe2b54a08732209
kk <44739720+kkAtGithub@users.noreply.github.com>
2021-04-22 10:53:24 +0800

    Update clone_repo.sh

--- clone_repo.sh
+++ clone_repo.sh
@@ -6,8 +6,8 @@ curl -o docker_setup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/
 chmod 700 docker_setup.sh
 curl -o fail2ban_setup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/fail2ban_setup.sh && \
 chmod 700 fail2ban_setup.sh
-curl -o wireguard_config.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/wireguard_config.sh && \
-chmod 700 wireguard_config.sh
+curl -o wireguard_config.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/wgcf_config.sh && \
+chmod 700 wgcf_config.sh
 curl -o iptables_config.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/iptables_config.sh && \
 chmod 700 iptables_config.sh
 curl -o clone_repo.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/clone_repo.sh && \
