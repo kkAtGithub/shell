1a0333eb98089dbd08802e823390329c14a09f7b
kk <zky.nais@gmail.com>
2021-04-25 17:49:10 +0800

    Update clone_repo.sh

--- clone_repo.sh
+++ clone_repo.sh
@@ -2,6 +2,8 @@
 
 cd /root/shell
 
+curl -o frpc_update.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/frpc_update.sh && \
+chmod 700 frpc_update.sh
 curl -o docker_setup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/docker_setup.sh && \
 chmod 700 docker_setup.sh
 curl -o fail2ban_setup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/fail2ban_setup.sh && \
@@ -12,5 +14,6 @@ curl -o iptables_config.sh https://raw.githubusercontent.com/kkAtGithub/shell/ma
 chmod 700 iptables_config.sh
 curl -o clone_repo.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/clone_repo.sh && \
 chmod 700 clone_repo.sh
+
 
 exit 0
