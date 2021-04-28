76d875e79b25866a99d7a6b8c42e28838976276d
kk <zky.nais@gmail.com>
2021-04-28 21:14:49 +0800

    Update clone_repo.sh

--- clone_repo.sh
+++ clone_repo.sh
@@ -8,7 +8,7 @@ curl -o docker_setup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/
 chmod 700 docker_setup.sh
 curl -o fail2ban_setup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/fail2ban_setup.sh && \
 chmod 700 fail2ban_setup.sh
-curl -o wireguard_config.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/wgcf_config.sh && \
+curl -o wgcf_config.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/wgcf_config.sh && \
 chmod 700 wgcf_config.sh
 curl -o iptables_config.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/iptables_config.sh && \
 chmod 700 iptables_config.sh
