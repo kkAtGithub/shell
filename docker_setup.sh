1bc9f714b7695eb0750a67eff9a8703ea397bf3b
kk <zky.nais@gmail.com>
2021-05-04 17:58:17 +0800

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -21,7 +21,7 @@ apt-get update && \
 
 apt-get install docker-ce docker-ce-cli containerd.io -y && \
 
-curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
 chmod +x /usr/local/bin/docker-compose
 
 mkdir /root/docker
