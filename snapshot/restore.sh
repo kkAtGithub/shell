2dcf317e11cd5151ffc4c50bad412f4d7cc5700b
kk <zky.nais@gmail.com>
2021-08-10 02:12:17 +0100

    Update restore.sh

--- snapshot/restore.sh
+++ snapshot/restore.sh
@@ -2,17 +2,17 @@
 
 chmod 700 /root/shell/clone_repo.sh && \
 /root/shell/clone_repo.sh && \
-/root/shell/iptables_config.sh -d 
+/root/shell/misc/iptables_config.sh -cd
 
 apt update && \
 apt install net-tools wireguard-tools &&\
-
+/bin/cp -rf /root/service/wg-quick@.service /lib/systemd/system/wg-quick@.service && \
 /bin/cp -rf /root/wireguard /etc/wireguard && \
 systemctl enable wg-quick@wg99 && \
 systemctl start wg-quick@wg99
 
-/root/shell/docker_setup.sh && \
-/bin/cp -rf /root/docker/docker.service /lib/systemd/system/docker.service && \
+/root/shell/init/docker_setup.sh && \
+/bin/cp -rf /root/service/docker.service /lib/systemd/system/docker.service && \
 systemctl daemon-reload docker.service && \
 systemctl restart docker.service && \
 /root/docker/docker-autostart.sh
