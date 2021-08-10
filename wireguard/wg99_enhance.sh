a5e20ab7307b6e949350312a2de1ac023e68cd25
kk <zky.nais@gmail.com>
2021-08-10 02:57:32 +0100

    Update wg99_enhance.sh

--- wireguard/wg99_enhance.sh
+++ wireguard/wg99_enhance.sh
@@ -1,16 +1,20 @@
 #!/bin/bash 
 
-if [ ! -f "/root/shell/wireguard/wg99_postdown.sh" ];then
-  sed -i '4a\PostDown = /root/shell/wireguard/wg99_postdown.sh' /etc/wireguard/wg99.conf
+if [ -f "/root/shell/wireguard/wg99_postdown.sh" ];then
+  sed -i '/PostDown = /d' /etc/wireguard/wg99.conf
+  sed -i '5a\PostDown = /root/shell/wireguard/wg99_postdown.sh' /etc/wireguard/wg99.conf
 fi
-if [ ! -f "/root/shell/wireguard/wg99_predown.sh" ];then
-  sed -i '4a\PreDown = /root/shell/wireguard/wg99_predown.sh' /etc/wireguard/wg99.conf
+if [ -f "/root/shell/wireguard/wg99_predown.sh" ];then
+  sed -i '/PreDown = /d' /etc/wireguard/wg99.conf
+  sed -i '5a\PreDown = /root/shell/wireguard/wg99_predown.sh' /etc/wireguard/wg99.conf
 fi
-if [ ! -f "/root/shell/wireguard/wg99_postup.sh" ];then
-  sed -i '4a\PostUp = /root/shell/wireguard/wg99_postup.sh' /etc/wireguard/wg99.conf
+if [ -f "/root/shell/wireguard/wg99_postup.sh" ];then
+  sed -i '/PostUp = /d' /etc/wireguard/wg99.conf
+  sed -i '5a\PostUp = /root/shell/wireguard/wg99_postup.sh' /etc/wireguard/wg99.conf
 fi
-if [ ! -f "/root/shell/wireguard/wg99_preup.sh" ];then
-  sed -i '4a\PreUp = /root/shell/wireguard/wg99_preup.sh' /etc/wireguard/wg99.conf
+if [ -f "/root/shell/wireguard/wg99_preup.sh" ];then
+  sed -i '/PreUp = /d' /etc/wireguard/wg99.conf
+  sed -i '5a\PreUp = /root/shell/wireguard/wg99_preup.sh' /etc/wireguard/wg99.conf
 fi
 
 systemctl restart wg-quick@wg99.service
