edac925ab316fd7f8e8ef14d90d15d909daab992
kk <zky.nais@gmail.com>
2021-08-10 02:07:28 +0100

    Update clone_repo.sh

--- clone_repo.sh
+++ clone_repo.sh
@@ -2,22 +2,10 @@
 
 cd /root/shell
 
-curl -o backup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/backup.sh && \
-chmod 700 backup.sh
-curl -o restore.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/restore.sh && \
-chmod 700 restore.sh
-curl -o frp_update.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/frp_update.sh && \
-chmod 700 frp_update.sh
-curl -o docker_setup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/docker_setup.sh && \
-chmod 700 docker_setup.sh
-curl -o fail2ban_setup.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/fail2ban_setup.sh && \
-chmod 700 fail2ban_setup.sh
-curl -o wgcf_config.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/wgcf_config.sh && \
-chmod 700 wgcf_config.sh
-curl -o iptables_config.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/iptables_config.sh && \
-chmod 700 iptables_config.sh
-curl -o clone_repo.sh https://raw.githubusercontent.com/kkAtGithub/shell/main/clone_repo.sh && \
-chmod 700 clone_repo.sh
+rm -rf ./*
 
+git clone https://github.com/kkAtGithub/shell.git || \
+(apt install git -y && git clone https://github.com/kkAtGithub/shell.git) && \
+chmod 700 -R ./
 
 exit 0
