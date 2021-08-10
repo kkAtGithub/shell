bfc5ec692b5d730cf10238e24c9e1424f121c085
kk <zky.nais@gmail.com>
2021-08-10 02:09:13 +0100

    Update backup.sh

--- snapshot/backup.sh
+++ snapshot/backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
 /bin/cp -rf /etc/wireguard /root/wireguard
-/bin/cp -rf /lib/systemd/system/docker.service /root/docker/docker.service
+/bin/cp -rf /lib/systemd/system/wg-quick@.service /root/service/wg-quick@.service
+/bin/cp -rf /lib/systemd/system/docker.service /root/service/docker.service
 
 exit 0
