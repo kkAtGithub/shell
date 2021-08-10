cf8d01642b79ab5720aecee6ae1f4ef10f5de84a
kk <zky.nais@gmail.com>
2021-08-10 02:44:36 +0100

    Update clone_repo.sh

--- clone_repo.sh
+++ clone_repo.sh
@@ -4,8 +4,8 @@ cd /root/shell
 
 rm -rf ./*
 
-git clone https://github.com/kkAtGithub/shell.git || \
-(apt install git -y && git clone https://github.com/kkAtGithub/shell.git) && \
+git clone https://github.com/kkAtGithub/shell.git ./ || \
+(apt install git -y && git clone https://github.com/kkAtGithub/shell.git ./) && \
 chmod 700 -R ./
 
 exit 0
