0254ea2743f1230660c5761805f71456f6f1c4ff
kk <zky.nais@gmail.com>
2021-08-11 00:12:17 +0100

    Update wgcf_setup.sh

--- init/wgcf_setup.sh
+++ init/wgcf_setup.sh
@@ -17,12 +17,14 @@ cd /root/wgcf
 
 curl -fsSL git.io/wgcf.sh | bash
 
-wgcf register && wgcf generate 
+wgcf register && wgcf generate && \
+sed -i '/AllowedIPs = 0.0.0.0\/0/d' wgcf-profile.conf && \
+sed -i '/AllowedIPs = ::\/0/d' wgcf-profile.conf
 
 if [ "$MODE" == "--ipv4" ];then
     sed -i '6a\PostDown = iptables -P FORWARD DROP' wgcf-profile.conf && \
     sed -i '6a\PreUp = iptables -P FORWARD ACCEPT' wgcf-profile.conf && \
-    sed -i '/AllowedIPs = ::\/0/d' wgcf-profile.conf && \
+    sed -i '/Endpoint = /i AllowedIPs = 0.0.0.0\/0' wgcf-profile.conf && \
     sed -i 's/engage.cloudflareclient.com/[2606:4700:d0::a29f:c001]/' wgcf-profile.conf && \
     sed -i 's/DNS = 1.1.1.1/DNS = 2620:119:35::35,2001:4860:4860::8888,2606:4700:4700::1111/' wgcf-profile.conf
 fi
@@ -30,7 +32,7 @@ fi
 if [ "$MODE" == "--ipv6" ];then
     sed -i '6a\PostDown = ip6tables -P FORWARD DROP' wgcf-profile.conf && \
     sed -i '6a\PreUp = ip6tables -P FORWARD ACCEPT' wgcf-profile.conf && \
-    sed -i '/AllowedIPs = 0.0.0.0\/0/d' wgcf-profile.conf && \
+    sed -i '/Endpoint = /i AllowedIPs = ::\/0' wgcf-profile.conf && \
     sed -i 's/engage.cloudflareclient.com/162.159.192.1/' wgcf-profile.conf && \
     sed -i 's/DNS = 1.1.1.1/DNS = 208.67.222.222,8.8.8.8,1.1.1.1/' wgcf-profile.conf
 fi
