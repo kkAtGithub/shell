a7b7ab0de67ceec470af91eda7208d2f358b9d97
kk <zky.nais@gmail.com>
2021-08-11 12:01:45 +0100

    Update restore.sh

--- snapshot/restore.sh
+++ snapshot/restore.sh
@@ -5,15 +5,20 @@ chmod 700 /root/shell/clone_repo.sh && \
 /root/shell/init/fail2ban_setup.sh && \
 /root/shell/misc/iptables_config.sh -c
 
-apt update && \
-apt install net-tools wireguard-tools &&\
-/bin/cp -rf /root/service/wg-quick@.service /lib/systemd/system/wg-quick@.service && \
-/bin/cp -rf /root/wireguard /etc/wireguard && \
-systemctl enable wg-quick@wg99 && \
-systemctl start wg-quick@wg99
+if [ -d "/root/backup/wireguard" ]; then
+    apt update && \
+    apt install net-tools wireguard-tools &&\
+    /bin/cp -rf /root/backup/service/wg-quick@.service /lib/systemd/system/wg-quick@.service && \
+    /bin/cp -rf /root/backup/wireguard /etc/wireguard && \
+    systemctl enable wg-quick@wg99 && \
+    systemctl start wg-quick@wg99
+fi
+
+/bin/cp -rf /root/backup/conf/hosts /etc/hosts
+/bin/cp -rf /root/backup/conf/sshd_config /etc/ssh/sshd_config
 
 /root/shell/init/docker_setup.sh && \
-/bin/cp -rf /root/service/docker.service /lib/systemd/system/docker.service && \
+/bin/cp -rf /root/backup/service/docker.service /lib/systemd/system/docker.service && \
 systemctl daemon-reload docker.service && \
 systemctl restart docker.service && \
 /root/docker/docker-autostart.sh
