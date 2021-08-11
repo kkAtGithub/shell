beb972ab54815c515dc6e4c23b7fe565978b9b5b
kk <zky.nais@gmail.com>
2021-08-11 12:01:48 +0100

    Update backup.sh

--- snapshot/backup.sh
+++ snapshot/backup.sh
@@ -1,7 +1,18 @@
 #!/bin/bash 
 
-/bin/cp -rf /etc/wireguard /root/wireguard
-/bin/cp -rf /lib/systemd/system/wg-quick@.service /root/service/wg-quick@.service
-/bin/cp -rf /lib/systemd/system/docker.service /root/service/docker.service
+if [ ! -d "/root/backup" ]; then
+    mkdir /root/backup
+    mkdir /root/backup/conf
+    mkdir /root/backup/service
+fi
+
+if [ -d "/etc/wireguard" ]; then
+    /bin/cp -rf /etc/wireguard /root/backup/wireguard
+    /bin/cp -rf /lib/systemd/system/wg-quick@.service /root/backup/service/wg-quick@.service
+fi
+
+/bin/cp -rf /lib/systemd/system/docker.service /root/backup/service/docker.service
+/bin/cp -rf /etc/hosts /root/backup/conf/hosts
+/bin/cp -rf /etc/ssh/sshd_config /root/backup/conf/sshd_config
 
 exit 0
