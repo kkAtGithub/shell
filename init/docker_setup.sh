c6d6239defadb8a85c6b293466f566e0512080bc
kk <zky.nais@gmail.com>
2021-08-15 03:01:58 +0100

    Update docker_setup.sh

--- init/docker_setup.sh
+++ init/docker_setup.sh
@@ -30,8 +30,10 @@ echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$ID \
     $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null && \
 
-apt-get update && \
-apt-get install docker-ce docker-ce-cli containerd.io -y && \
+(apt-get update && \
+apt-get install docker-ce docker-ce-cli containerd.io -y) || \
+apt-get install docker-ce-cli containerd.io -y && \
+apt-get install docker-ce -y
 
 if [ ! -d "/root/docker" ]; then
   mkdir /root/docker
