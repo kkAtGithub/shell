2008b0a667c3beec7496174ffa4c368cf5488e4f
kk <zky.nais@gmail.com>
2022-01-24 19:52:38 +0800

    Update fail2ban_setup.sh

--- init/fail2ban_setup.sh
+++ init/fail2ban_setup.sh
@@ -2,7 +2,7 @@
 
 apt-get install fail2ban inetutils-syslogd -y
 
-/bin/cp -rf /root/shell/misc/jail.local /etc/fail2ban/jail.local
+/bin/cp -rf /root/shell/fail2ban/ /etc/fail2ban/
 
 systemctl enable fail2ban && \
 systemctl restart fail2ban
