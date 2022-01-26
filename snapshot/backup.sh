27f7c137aa61b2d88f4d6e4137ce775d58693eb6
kk <zky.nais@gmail.com>
2022-01-26 13:44:46 +0800

    Update backup.sh

--- snapshot/backup.sh
+++ snapshot/backup.sh
@@ -14,11 +14,11 @@ fi
 if [ -d "/etc/wireguard" ]; then
     rm -rf /root/backup/wireguard
     /bin/cp -rf /etc/wireguard /root/backup/
-    /bin/cp -rf /lib/systemd/system/wg-quick@.service /root/backup/service/wg-quick@.service
+#     /bin/cp -rf /lib/systemd/system/wg-quick@.service /root/backup/service/wg-quick@.service
 fi
 
 if [ -d "/etc/docker" ]; then
-    /bin/cp -rf /lib/systemd/system/docker.service /root/backup/service/docker.service
+    /bin/cp -rf /etc/systemd/system/docker.service /root/backup/service/docker.service
 fi
 
 /bin/cp -rf /etc/hosts /root/backup/conf/hosts
