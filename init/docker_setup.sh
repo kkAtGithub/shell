03907f6a62979e0ed64c91a6f34e5dd2903a4dab
kk <zky.nais@gmail.com>
2023-03-12 18:57:36 +0800

    Update docker_setup.sh

--- init/docker_setup.sh
+++ init/docker_setup.sh
@@ -1,46 +1,43 @@
 #!/bin/bash
 
-# apt-get remove docker docker-engine docker.io containerd runc 
-
-# apt-get purge -y docker-engine docker docker.io docker-ce 
-# apt-get autoremove -y --purge docker-engine docker docker.io docker-ce 
-
-# rm -rf /var/lib/docker /etc/docker
-# groupdel docker
-# rm -rf /var/run/docker.sock
-
 apt-get update && \
 apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg \
+    iptables \
     lsb-release -y && \
 
 ID=$(echo $(lsb_release -is) | awk '{print tolower($0)}')
 MACHINE=$(uname -m)
-if [[ $MACHINE =~ "x86_64" ]]; then
-    ARCH="amd64"
-    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
-    chmod +x /usr/local/bin/docker-compose
-fi
-if [[ $MACHINE =~ "aarch64" ]]; then
-    ARCH="arm64"
-    apt-get update
-    apt-get install -y python3-pip libffi-dev
-    pip3 install docker-compose
-fi
+#if [[ $MACHINE =~ "x86_64" ]]; then
+#    ARCH="amd64"
+#    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+#    chmod +x /usr/local/bin/docker-compose
+#fi
+#if [[ $MACHINE =~ "aarch64" ]]; then
+#    ARCH="arm64"
+#    apt-get update
+#    apt-get install -y python3-pip libffi-dev
+#    pip3 install docker-compose
+#fi
 
-curl -fsSL https://download.docker.com/linux/$ID/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && \
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$ID \
-    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+chmod a+r /etc/apt/keyrings/docker.gpg
 
 (apt-get update && \
-apt-get install docker-ce docker-ce-cli containerd.io -y) || \
-apt-get install docker-ce docker-ce-cli containerd.io -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y) || \
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 apt-get install docker-ce -y
+
+apt-get install docker-compose-plugin -y
 
 if [ ! -d "/root/docker" ]; then
   mkdir /root/docker
@@ -60,11 +57,6 @@ echo "docker-compose -f /root/docker/docker-compose.yml down --remove-orphans" >
 echo "exit 0" >> docker_down.sh && \
 chmod 700 docker_down.sh
 
-if [ -f "/root/docker/docker-autostart.sh" ]; then
-    rm /root/docker/docker-autostart.sh
-    crontab -l -u root | sed '/docker/d' | crontab -u root -
-fi
-
 echo "[Unit]" > /etc/systemd/system/docker-stack.service && \
 echo "Description=Docker stack" >> /etc/systemd/system/docker-stack.service && \
 echo "Wants=network-online.target" >> /etc/systemd/system/docker-stack.service && \
@@ -82,6 +74,6 @@ echo "WantedBy=multi-user.target" >> /etc/systemd/system/docker-stack.service &&
 echo "" >> /etc/systemd/system/docker-stack.service && \
 
 systemctl daemon-reload && \
-systemctl enable docker-stack.service 
+systemctl enable docker-stack.service
 
 exit 0
